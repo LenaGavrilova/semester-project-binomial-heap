c20e44de6b01ee49fcf955a157e6b7bc30481551
SRVergasov <SRVergasov@stud.kpfu.ru>
2022-04-13 19:33:09 +0300

    completed Merge method divided him (now its just function BinomialHeapUnion in data_structure.hpp)

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -31,10 +31,9 @@ namespace itis {
     }
 
   struct BinomialHeap {
-  protected:
+  public:
       int size_{0};
-      int capacity_{0};
-      std::list<Node*> root_list;
+      Node* root_{nullptr};
 
   public:
 
@@ -61,7 +60,7 @@ namespace itis {
        * @param heap2 - куча 2
        * @return true если слились и false если разлились
        */
-      BinomialHeap* Merge(BinomialHeap heap1, BinomialHeap heap2);
+      BinomialHeap* Root_List_Merge(BinomialHeap heap1, BinomialHeap heap2);
 
       /**
        * пока не понял че за метод такой интересный
@@ -72,9 +71,64 @@ namespace itis {
 
       // геттеры
 
-      int capacity() const;
-
       int size() const;
   };
+
+    BinomialHeap* Root_List_Merge(BinomialHeap heap1, BinomialHeap heap2) {
+        Node *h;
+        Node *h1 = heap1.root_;
+        Node *h2 = heap2.root_;
+        while (h1 != nullptr && h2 != nullptr) {
+            if (h1->degree <= h2->degree) {
+                h->sibling = h1;;
+                h1 = h1->sibling;
+            } else {
+                h->sibling = h2;
+                h2 = h2->sibling;
+            }
+        }
+        while (h1 != nullptr) {
+            h->sibling = h1;
+            h1 = h1->sibling;
+        }
+        while (h2 != nullptr) {
+            h->sibling = h2;
+            h2 = h2->sibling;
+        }
+        BinomialHeap* binomialHeap = new BinomialHeap();
+        binomialHeap->root_ = h;
+        binomialHeap->size_ = heap1.size_ + heap2.size_;
+        return binomialHeap;
+    }
+
+    BinomialHeap* BinomialHeapUnion(BinomialHeap heap1, BinomialHeap heap2) {
+        Node* h = Root_List_Merge(heap1, heap2)->root_;
+        Node* prev_x = nullptr;
+        Node* x = h;
+        Node* next_x = x->sibling;
+        while (next_x != nullptr) {
+            if (x->degree != next_x->degree ||
+                (next_x->sibling != nullptr and next_x->sibling->degree == x->degree)) {
+                prev_x = x;
+                x = next_x;
+            } else if (x->key <= next_x->key) {
+                x->sibling = next_x->sibling;
+                binomialTreeLink(next_x, x);
+            } else {
+                if (prev_x == nullptr) {
+                    h = next_x;
+                } else {
+                    prev_x->sibling = next_x;
+                }
+                binomialTreeLink(x, next_x);
+                x = next_x;
+            }
+            next_x = x->sibling;
+        }
+        BinomialHeap* binomialHeap = new BinomialHeap();
+        binomialHeap->root_ = h;
+        binomialHeap->size_ = heap1.size_ + heap2.size_;
+        return binomialHeap;
+    }
 
 }  // namespace itis
--- src/data_structure.cpp
+++ src/data_structure.cpp
@@ -16,8 +16,8 @@ namespace itis {
     }
 
     int BinomialHeap::Minimum() const {
-        int minKey = root_list.front()->key;
-        Node* node{root_list.front()->sibling};
+        int minKey = root_->key;
+        Node* node{root_->sibling};
         while (node != nullptr) {
             if (node->key < minKey) {
                 minKey = node->key;
@@ -31,36 +31,7 @@ namespace itis {
         return std::nullopt;
     }
 
-    BinomialHeap* BinomialHeap::Merge(BinomialHeap heap1, BinomialHeap heap2) {
-        std::list<Node*> h;
-        Node* h1 = heap1.root_list.front();
-        Node* h2 = heap2.root_list.front();
-        while (h1 != nullptr && h2 != nullptr) {
-            if (h1->degree <= h2->degree) {
-                h.back()->sibling = h1;
-                h.push_back(h1);
-                h1 = h1->sibling;
-            } else {
-                h.back()->sibling = h2;
-                h.push_back(h2);
-                h2 = h2->sibling;
-            }
-        }
-        while (h1 != nullptr) {
-            h.back()->sibling = h1;
-            h.push_back(h1);
-            h1 = h1->sibling;
-        }
-        while (h2 != nullptr) {
-            h.back()->sibling = h2;
-            h.push_back(h2);
-            h2 = h2->sibling;
-        }
-        BinomialHeap* binomialHeap = new BinomialHeap();
-        binomialHeap->root_list = h;
-        // тут пока что чисто первое условие биномиальной кучи выполняется
-        return binomialHeap;
-    }
+
 
     void BinomialHeap::Decrease() {
 
@@ -70,13 +41,8 @@ namespace itis {
 
     }
 
-    int BinomialHeap::capacity() const {
-        return capacity_;
-    }
-
     int BinomialHeap::size() const {
         return size_;
     }
-
 
 }  // namespace itis
