a628db4874beec722ef863aaf945e8ae0344cc26
Ksenia Katargina <ksekat01@mail.ru>
2022-04-17 00:43:20 +0300

    Add generate.py

---
+++ dataset/generate.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+import os
+
+DATASETS_SIZES = [100, 1000, 10_000, 100_000, 1_000_000]
+DATASETS_NAMES = ["01", "02", "03", "04", "05"]
+OPERATIONS_NAMES = ["Insert", "ExtractMin", "Merge"]
+
+
+def make_directories(name):
+    try:
+        os.mkdir("data/{}".format(name))
+        for i in DATASETS_NAMES:
+            os.mkdir("data/{}/{}".format(name, i))
+    except FileExistsError:
+        print("delete old dataset before using our")
+
+
+if __name__ == "__main__":
+    for i in OPERATIONS_NAMES:
+        make_directories(i)
+
+    for dataset_name in DATASETS_NAMES:
+        for dataset_size in DATASETS_SIZES:
+            subprocess.Popen([sys.executable, "generate_csv_dataset.py",
+                              "data/{}/{}/{}.csv".format("ExtractMin", dataset_name, dataset_size),
+                              "--samples", "{}".format(dataset_size)])
